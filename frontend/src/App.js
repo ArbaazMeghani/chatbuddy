84922261b1ab14e969d921ce9e5069983ad4e6dd
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-05-24 18:41:53 -0500

    Add some styling and create a layout for the chat application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,36 @@
 import React from "react";
 import socketIOClient from "socket.io-client";
 import keys from "./config/keys"
-import { Button, TextField } from "@material-ui/core";
+import { Button, TextField, Grid } from "@material-ui/core";
+import SendIcon from '@material-ui/icons/Send';
+import './styles/App.css'
 
 class App extends React.Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      message: "",
+      messages: [],
+      id: 0
+    }
+  }
 
   componentDidMount() {
     const socket = socketIOClient(keys.CHAT_SERVICE_URL);
     socket.on("chat-message", data => {
       console.log(data)
-      this.setState({message: data})
+      this.setState({receivedMessage: data})
     });
+  }
+
+  publishMessage = (message) => {
+    this.setState({
+    messages: [...this.state.messages, <div key={this.state.id}>{message}}</div>],
+      id: this.state.id + 1,
+      message: ""
+    })
   }
 
   handleChange = (event) => {
@@ -19,16 +39,35 @@ class App extends React.Component {
 
   handleSend = (event) => {
     event.preventDefault()
+    if(this.state.message === "") {
+      return
+    }
+
     console.log("send button clicked with message: " + this.state.message)
+    this.publishMessage(this.state.message)
   }
 
   render() {
+    console.log(this.state.messages)
     return (
-      <div>
-        <form onSubmit={this.handleSend}>
-          <TextField id="outlined-basic" label="message" variant="outlined" onChange={this.handleChange}/>
-          <Button type="submit">Send</Button>
-        </form>
+      <div className="root">
+        <div className="message-container">
+          {this.state.messages}
+        </div>
+        <div className="message-composer">
+          <form onSubmit={this.handleSend}>
+            <Grid container spacing={0} alignItems="center">
+              <Grid item xs={10}>
+                <TextField className="message-text-box" id="outlined-basic" value={this.state.message} placeholder="message" variant="outlined" onChange={this.handleChange}/>
+              </Grid>
+              <Grid item xs={2}>
+                <Button type="submit" variant="contained" color="primary" endIcon={<SendIcon/>}>
+                  Send
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
       </div>
     );
   }
