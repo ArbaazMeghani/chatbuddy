ca1b54cddac9ae9508adcde0ca91a695f784d592
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-05-24 15:29:08 -0500

    Basic frontend form setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,37 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import socketIOClient from "socket.io-client";
 import keys from "./config/keys"
+import { Button, TextField } from "@material-ui/core";
 
-function App() {
-  const [response, setResponse] = useState("");
+class App extends React.Component {
 
-  useEffect(() => {
+  componentDidMount() {
     const socket = socketIOClient(keys.CHAT_SERVICE_URL);
     socket.on("chat-message", data => {
       console.log(data)
-      setResponse(data);
+      this.setState({message: data})
     });
-  }, []);
+  }
 
-  return (
-    <p>
-      It's {response}
-    </p>
-  );
+  handleChange = (event) => {
+    this.setState({message: event.target.value})
+  }
+
+  handleSend = (event) => {
+    event.preventDefault()
+    console.log("send button clicked with message: " + this.state.message)
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSend}>
+          <TextField id="outlined-basic" label="message" variant="outlined" onChange={this.handleChange}/>
+          <Button type="submit">Send</Button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
