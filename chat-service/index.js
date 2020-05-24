a0ef5dc4d56a836f4f3459f1097baa963bc02e21
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-05-24 12:15:21 -0500

    Create socket.io in chat-service and add html for testing purposes

--- chat-service/index.js
+++ chat-service/index.js
@@ -1,15 +1,9 @@
-const express = require('express')
 const keys = require('./config/keys')
-const authorization = require('authorization')
+const io = require('socket.io')(keys.PORT)
 
-const app = express()
-app.use(express.json())
-
-const verifyToken = authorization.verifyToken(keys.JWT_SECRET)
-
-app.get('/', verifyToken, (req, res) => {
-  res.status(200).send("Success")
+io.on("connection", socket => {
+  console.log("user connected")
+  socket.emit("chat-message", "hello world")
 })
 
-console.log("Chat-Service: Running Express Server on Port: " + keys.PORT)
-app.listen(keys.PORT)
+console.log("Chat-Service: Running Server on Port: " + keys.PORT)
