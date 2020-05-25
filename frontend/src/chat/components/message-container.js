0b80358c9deea589930539be6195293dc8d72a00
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-05-25 15:32:53 -0500

    Remove useless constructors

--- frontend/src/chat/components/message-container.js
+++ frontend/src/chat/components/message-container.js
@@ -2,10 +2,6 @@ import React from 'react'
 import '../styles/chat.css'
 
 class MessageContainer extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
   render() {
     return (
       <div className="message-container">
