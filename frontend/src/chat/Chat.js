6c79f47f87366cf0121192db5aaa4f22659a26a6
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-05-25 17:19:57 -0500

    Create message composer component

--- frontend/src/chat/Chat.js
+++ frontend/src/chat/Chat.js
@@ -1,25 +1,32 @@
 import React from "react"
 import socketIOClient from "socket.io-client"
 import keys from "../config/keys"
-import { Button, TextField, Grid } from "@material-ui/core"
-import SendIcon from '@material-ui/icons/Send'
 import Message from './components/message'
 import MessageContainer from './components/message-container'
+import MessageComposer from './components/message-composer'
 import './styles/chat.css'
 
 class Chat extends React.Component {
-
+  
   constructor() {
     super()
-
+    
     this.state = {
       socket: socketIOClient(keys.CHAT_SERVICE_URL),
-      message: "",
       messages: [],
       id: 0
     }
   }
-
+  
+  render() {
+    return (
+      <div className="root">
+        <MessageContainer messages={this.state.messages} />
+        <MessageComposer handleSend={this.handleSend} />
+      </div>
+    );
+  }
+  
   componentDidMount() {
     this.state.socket.on("chat-message", receivedMessage => {
       console.log(receivedMessage)
@@ -32,7 +39,6 @@ class Chat extends React.Component {
     this.setState({
       messages: [messageComponent, ...this.state.messages],
       id: this.state.id + 1,
-      message: ""
     })
   }
 
@@ -40,46 +46,11 @@ class Chat extends React.Component {
     this.state.socket.emit("chat-message", message)
   }
 
-  handleChange = (event) => {
-    this.setState({message: event.target.value})
-  }
-
-  handleSend = (event) => {
-    event.preventDefault()
-    const message = this.state.message
-
-    if(message === "") {
-      return
-    }
-
-    console.log("send button clicked with message: " + message)
-
+  handleSend = (message) => {
     this.publishMessage(message, "sender")
     this.broadcastMessage(message)
   }
 
-  render() {
-    console.log(this.state.messages)
-    return (
-      <div className="root">
-        <MessageContainer messages={this.state.messages} />
-        <div className="message-composer">
-          <form onSubmit={this.handleSend}>
-            <Grid container spacing={0} alignItems="center">
-              <Grid item xs={10}>
-                <TextField className="message-text-box" id="outlined-basic" value={this.state.message} placeholder="message" variant="outlined" onChange={this.handleChange}/>
-              </Grid>
-              <Grid item xs={2}>
-                <Button type="submit" variant="contained" color="primary" endIcon={<SendIcon/>}>
-                  Send
-                </Button>
-              </Grid>
-            </Grid>
-          </form>
-        </div>
-      </div>
-    );
-  }
 }
 
 export default Chat;
---
+++ frontend/src/chat/components/message-composer.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Button, TextField, Grid } from "@material-ui/core"
+import SendIcon from '@material-ui/icons/Send'
+import '../styles/chat.css'
+
+class MessageComposer extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      message: ""
+    }
+  }
+
+  render() {
+    return (
+      <div className="message-composer">
+        <form onSubmit={this.handleSend}>
+          <Grid container spacing={0} alignItems="center">
+            <Grid item xs={10}>
+              <TextField className="message-text-box" id="outlined-basic" value={this.state.message} placeholder="message" variant="outlined" onChange={this.handleChange}/>
+            </Grid>
+            <Grid item xs={2}>
+              <Button type="submit" variant="contained" color="primary" endIcon={<SendIcon/>}>
+                Send
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    )
+  }
+
+  handleChange = (event) => {
+    this.setState({message: event.target.value})
+  }
+
+  handleSend = (event) => {
+    event.preventDefault()
+
+    if(this.state.message === "") {
+      return
+    }
+
+    console.log("send button clicked with message: " + this.state.message)
+
+    this.props.handleSend(this.state.message)
+    this.setState({
+      message: ""
+    })
+  }
+}
+
+export default MessageComposer
