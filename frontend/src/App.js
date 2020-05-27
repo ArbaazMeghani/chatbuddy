d194a4fc3d9b9cef795ddb000daea43e11335925
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-05-26 19:07:05 -0500

    Add react router to route user to authentication page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 import Chat from "./chat/chat";
+import Authentication from "./authentication/authentication"
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends React.Component {
   render() {
-    return <Chat />
+    return (
+      <Router>
+        <Route exact path="/" component={Chat} />
+        <Route path="/auth" component={Authentication} />
+      </Router>
+    )
   }
 }
 
---
+++ frontend/src/authentication/authentication.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Authentication extends React.Component {
+  render() {
+    return (
+      <div>
+        "Hello World"
+      </div>
+    )
+  }
+}
+
+export default Authentication
