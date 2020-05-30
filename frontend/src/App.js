fb453155f88ddeac95305d4c6bd33f86fc7ca610
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-05-30 09:31:38 -0500

    Check cookies to see if user is logged in and redirect accordingly

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,47 @@
 import React from "react";
 import Chat from "./chat/chat";
 import Authentication from "./authentication/authentication"
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import Cookies from 'js-cookie'
 
 class App extends React.Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      authToken: Cookies.get("authToken")
+    }
+  }
+
   render() {
     return (
       <Router>
-        <Route exact path="/" component={Chat} />
-        <Route path="/auth" component={Authentication} />
+        <this.protectedChat authToken={this.state.authToken}/>
+        <this.authentication authToken={this.state.authToken}/>
       </Router>
     )
+  }
+
+  protectedChat = ({authToken}) => {
+    if(!authToken) {
+      return <Redirect to="/auth" />
+    }
+
+    try {
+      return <Route exact path="/" component={Chat} />
+    } catch(err) {
+      Cookies.remove("authToken")
+      return <Redirect to="/auth" />
+    }
+  }
+
+  authentication = ({authToken}) => {
+    if(authToken) {
+      return <Redirect to="/" />
+    }
+
+    return <Route path="/auth" component={Authentication} />
   }
 }
 
